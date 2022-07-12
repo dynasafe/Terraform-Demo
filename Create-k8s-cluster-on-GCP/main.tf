8a3af9b7fe7063990c829e25945343d60d446ef2
williamsh.huang <tisamwilliam@gmail.com>
2022-07-12 16:26:30 +0800

    Fix multiple static IP problem

--- Create-k8s-cluster-on-GCP/main.tf
+++ Create-k8s-cluster-on-GCP/main.tf
@@ -7,11 +7,6 @@ data "google_compute_image" "centos_image" {
   project = "centos-cloud"
 }
 
-resource "google_compute_address" "static" {
-  name = "ipv4-address"
-  region  = "asia-east1"
-}
-
 resource "google_compute_instance" "control-plane" {
   count        = 1
 
@@ -19,12 +14,6 @@ resource "google_compute_instance" "control-plane" {
   machine_type = "e2-mediam" # 2core/4GB
   zone         = "us-central1"
 
-  # network_interface {
-  #   network = "default"
-  #   access_config {
-  #     nat_ip = google_compute_address.static.address
-  #   }
-  # }
 
   boot_disk {
     initialize_params {
@@ -33,26 +22,12 @@ resource "google_compute_instance" "control-plane" {
   }
 }
 
-# VPC to dns?
-resource "google_compute_address" "static" {
-  name = "ipv4-address"
-  region  = "us-central1"
-}
-
 resource "google_compute_instance" "worker-node" {
   count        = 2
 
   name         = "terraform-version-control-${count.index}"
   machine_type = "custom-1-4096" # 1core/4GB
   zone         = "asia-east1-a"
-
-# VPC to dns?
-  # network_interface {
-  #   network = "default"
-  #   access_config {
-  #     nat_ip = google_compute_address.static.address
-  #   }
-  # }
 
   boot_disk {
     initialize_params {
