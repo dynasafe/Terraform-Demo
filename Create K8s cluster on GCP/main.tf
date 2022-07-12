8d514b519f86f07c0bfc4f14867558ff5a05c9dd
williamsh.huang <tisamwilliam@gmail.com>
2022-07-12 16:07:42 +0800

    Feat create 1 control 2 worker on GCP

--- Create K8s cluster on GCP/main.tf
+++ Create K8s cluster on GCP/main.tf
@@ -12,19 +12,47 @@ resource "google_compute_address" "static" {
   region  = "asia-east1"
 }
 
-resource "google_compute_instance" "default" {
-  count        = 0 
+resource "google_compute_instance" "control-plane" {
+  count        = 1
 
   name         = "terraform-version-control-${count.index}"
-  machine_type = "e2-micro"
-  zone         = "asia-east1-a"
+  machine_type = "e2-mediam" # 2core/4GB
+  zone         = "us-central1"
 
-  network_interface {
-    network = "default"
-    access_config {
-      nat_ip = google_compute_address.static.address
+  # network_interface {
+  #   network = "default"
+  #   access_config {
+  #     nat_ip = google_compute_address.static.address
+  #   }
+  # }
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.centos_image.self_link
     }
   }
+}
+
+# VPC to dns?
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "worker-node" {
+  count        = 2
+
+  name         = "terraform-version-control-${count.index}"
+  machine_type = "custom-1-4096" # 1core/4GB
+  zone         = "asia-east1-a"
+
+# VPC to dns?
+  # network_interface {
+  #   network = "default"
+  #   access_config {
+  #     nat_ip = google_compute_address.static.address
+  #   }
+  # }
 
   boot_disk {
     initialize_params {
